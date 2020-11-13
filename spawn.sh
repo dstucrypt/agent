737b716fd441db3d9c1eee4ae564aba474ac75df
Illya Petrov <ilya.muromec@gmail.com>
2020-11-13 18:34:45 +0100

    specify number of cpus

--- spawn.sh
+++ spawn.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 control_port=3100
-low_port=3101
-high_port=3104
 
 respawn() {
   while true
@@ -18,18 +16,26 @@ stop() {
 
 
 key=$1
+num=$2
+if [ -z "$num" ] ;then
+num=4
+fi
+
 process=""
 if [ -z "$key" ] ; then
   echo "No key given" >> /dev/stderr
   exit 1;
 fi
-for port in $(seq -w $low_port $high_port)
+
+for x in $(seq 1 $num)
 do
-  respawn taskset --cpu-list $[ $port - $low_port ] node index.js --agent  --tcp --bind :$port  --connect_key $key   --only_known --ca_path CACertificates.p7b &
+  cpu=$[ $x - 1 ]
+  port=$[ $control_port + $x ]
+  respawn taskset --cpu-list $cpu node index.js --agent  --tcp --bind :$port  --connect_key $key   --only_known & #--ca_path CACertificates.p7b &
   process="$process $!"
 done
 
-respawn node index.js --proxy  --bind :$control_port --ports $low_port:$high_port --connect_key bdc7b38e820bd95d5a42bd3566638479332b6704332573af05ff931a1cdbda07  &
+respawn node index.js --proxy  --bind :$control_port --ports $[ $control_port + 1 ]:$[ $control_port + $num ] --connect_key bdc7b38e820bd95d5a42bd3566638479332b6704332573af05ff931a1cdbda07  &
 process="$process $!"
 
 
