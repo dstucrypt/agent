034d11ca6b51c89b054236c3f66001a2dfb8b34e
Illya Petrov <ilya.muromec@gmail.com>
2020-12-24 18:55:16 +0100

    prettier

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var argv = require('yargs')
-    .usage('Sign, encrypt or decrypt UASIGN files')
-    .option('tsp', {default: false})
-    .option('tax', {default: true})
-    .option('detached', {default: false})
-    .option('role', {default: 'director'})
-    .argv;
+var argv = require("yargs")
+  .usage("Sign, encrypt or decrypt UASIGN files")
+  .option("tsp", { default: false })
+  .option("tax", { default: true })
+  .option("detached", { default: false })
+  .option("role", { default: "director" }).argv;
 
-const agent = require('./agent');
-agent.main(argv)
-  .then((result)=> {
+const agent = require("./agent");
+agent
+  .main(argv)
+  .then((result) => {
     // daemon was started and is still running
-    if(typeof result === 'function') {
+    if (typeof result === "function") {
       return null;
     }
-    process.exit(result ? 0 : 1)
+    process.exit(result ? 0 : 1);
   })
-  .catch((error)=> {
-    if(!(error instanceof agent.ReadFileError)) {
-      console.error('Internal error');
+  .catch((error) => {
+    if (!(error instanceof agent.ReadFileError)) {
+      console.error("Internal error");
     }
-    if(argv.debug) {
-      console.error('Details', error);
+    if (argv.debug) {
+      console.error("Details", error);
     }
     process.exit(1);
   });
